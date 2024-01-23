2b18d560926e7606334f093e6086e67f5367051f
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-01-23 17:13:17 +0300

    add about api and cruds

---
+++ Presentation/WebApi/Controllers/AboutController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Features.CQRS.Commands.AboutCommands;
+using Application.Features.CQRS.Handlers.AboutHandlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly CreateAboutCommandHandler _createAboutCommandHandler;
+
+        private readonly GetAboutByIdQueryHandler _getAboutByIdQueryHandler;
+        private readonly GetAboutQueryHandler _getAboutQueryHandler;
+
+        private readonly UpdateAboutCommandHandler _updateAboutCommandHandler;
+        private readonly RemoveAboutCommandHandler _removeAboutCommandHandler;
+
+        public AboutController(CreateAboutCommandHandler createAboutCommandHandler, GetAboutByIdQueryHandler getAboutByIdQueryHandler, GetAboutQueryHandler getAboutQueryHandler, UpdateAboutCommandHandler updateAboutCommandHandler, RemoveAboutCommandHandler removeAboutCommandHandler)
+        {
+            _createAboutCommandHandler = createAboutCommandHandler;
+            _getAboutByIdQueryHandler = getAboutByIdQueryHandler;
+            _getAboutQueryHandler = getAboutQueryHandler;
+            _updateAboutCommandHandler = updateAboutCommandHandler;
+            _removeAboutCommandHandler = removeAboutCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AboutList()
+        {
+            var values = await _getAboutQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAbout(int id)
+        {
+            var value = await _getAboutByIdQueryHandler.Handle(new Application.Features.CQRS.Queries.AboutQueries.GetAboutByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAbout(CreateAboutCommand command)
+        {
+            await _createAboutCommandHandler.Handle(command);
+            return Ok("About addded");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveAbout(int id)
+        {
+            await _removeAboutCommandHandler.Handle(new RemoveAboutCommand(id));
+            return Ok("About deleted");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAbaout(UpdateAboutCommand command)
+        {
+            await _updateAboutCommandHandler.Handle(command);
+            return Ok("About updated succesfully");
+        }
+    }
+}
--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -1,6 +1,22 @@
+using Application.Features.CQRS.Handlers.AboutHandlers;
+using Application.Interfaces;
+using Persistance.Context;
+using Persistance.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<RentCarContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+//CQRS HANDLERS
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

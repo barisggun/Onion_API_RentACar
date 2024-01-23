47b3365b8297d5209dc0750be07f925a330c04b4
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-01-23 16:45:37 +0300

    add about cqrs commands handlers queries

---
+++ Core/Application/Features/CQRS/Commands/AboutCommands/RemoveAboutCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Commands.AboutCommands
+{
+    public class RemoveAboutCommand
+    {
+        public int Id { get; set; }
+        public RemoveAboutCommand(int ıd)
+        {
+            Id = ıd;
+        }
+
+        //Çünkü remove işleminde bana bütün propertyler değil sadece id lazım.Constructor edilmiş ID
+
+
+    }
+}

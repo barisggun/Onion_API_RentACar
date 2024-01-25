9e3898527ccd27076d4879b5989a67406165488b
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-01-25 21:42:32 +0300

    add banner CRQS

---
+++ Core/Application/Features/CQRS/Handlers/BannerHandlers/GetBannerByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Features.CQRS.Queries.AboutQueries;
+using Application.Features.CQRS.Queries.BannerQueries;
+using Application.Features.CQRS.Results.AboutResults;
+using Application.Features.CQRS.Results.BannerResults;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.BannerHandlers
+{
+    public class GetBannerByIdQueryHandler
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public GetBannerByIdQueryHandler(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetBannerByIdQueryResult> Handle(GetBannerByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+
+            return new GetBannerByIdQueryResult
+            {
+                BannerID = values.BannerID,
+                Description = values.Description,
+                Title = values.Title,
+                VideoDescription = values.VideoDescription,
+                VideoUrl = values.VideoUrl
+            };
+        }
+    }
+}
---
+++ Core/Application/Features/CQRS/Handlers/BannerHandlers/UpdateBannerCommandHandle.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.CQRS.Commands.AboutCommands;
+using Application.Features.CQRS.Commands.BannerCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.BannerHandlers
+{
+    public class UpdateBannerCommandHandle
+    {
+        private readonly IRepository<Banner> _repository;
+
+        public UpdateBannerCommandHandle(IRepository<Banner> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateBannerCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.BannerID);
+
+            values.Description = command.Description;
+            values.Title = command.Title;
+            values.VideoUrl = command.VideoUrl;
+            values.VideoDescription = command.VideoDescription;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}

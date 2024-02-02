cdbd611a130a650db32a1648d8e02ba75b46fd62
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-02-02 12:21:03 +0300

    add brand cqrs

---
+++ Core/Application/Features/CQRS/Handlers/BrandHandlers/GetBrandQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Features.CQRS.Handlers.BannerHandlers;
+using Application.Features.CQRS.Results.BannerResults;
+using Application.Features.CQRS.Results.BrandResults;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.BrandHandlers
+{
+    public class GetBrandQueryHandler
+    {
+        private readonly IRepository<Brand> _repository;
+
+        public GetBrandQueryHandler(IRepository<Brand> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetBrandQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetBrandQueryResult
+            {
+                    BrandID = x.BrandID,
+                    Name = x.Name
+
+            }).ToList();
+        }
+    }
+}

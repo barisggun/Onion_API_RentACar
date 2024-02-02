e338b4c8ae2603926d20f42a92c70c8b135c2482
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-02-02 18:34:51 +0300

    add contact cqrs

---
+++ Core/Application/Features/CQRS/Handlers/ContactHandlers/GetContactQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Features.CQRS.Results.ContactResults;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.ContactHandlers
+{
+    public class GetContactQueryHandler
+    {
+        private readonly IRepository<Contact> _repository;
+
+        public GetContactQueryHandler(IRepository<Contact> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetContactQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetContactQueryResult
+            {
+                ContactID = x.ContactID,
+                Name = x.Name,
+                Email = x.Email,
+                SendDate = x.SendDate,
+                Subjects = x.Subjects,
+                Message = x.Message
+
+            }).ToList();
+        }
+    }
+}
---
+++ Core/Application/Features/CQRS/Handlers/ContactHandlers/UpdateContactCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.CQRS.Commands.ContactCommands;
+using Application.Interfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.CQRS.Handlers.ContactHandlers
+{
+    public class UpdateContactCommandHandler
+    {
+        private readonly IRepository<Contact> _repository;
+
+        public UpdateContactCommandHandler(IRepository<Contact> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateContactCommand command)
+        {
+            var values = await _repository.GetByIdAsync(command.ContactID);
+
+            values.Name = command.Name;
+            values.Email = command.Email;
+            values.SendDate = command.SendDate;
+            values.Subjects = command.Subjects;
+
+            await _repository.UpdateAsync(values);
+        }
+    }
+}

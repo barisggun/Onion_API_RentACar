d211461b30f6384272e6b7b250cb866895d02ab1
Barış Gün <108459412+bgun7@users.noreply.github.com>
2024-02-02 13:16:38 +0300

    add brand api controller and scopes

--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Application.Features.CQRS.Handlers.AboutHandlers;
 using Application.Features.CQRS.Handlers.BannerHandlers;
+using Application.Features.CQRS.Handlers.BrandHandlers;
 using Application.Interfaces;
 using Persistance.Context;
 using Persistance.Repositories;
@@ -24,6 +25,11 @@ builder.Services.AddScoped<CreateBannerCommandHandler>();
 builder.Services.AddScoped<UpdateBannerCommandHandle>();
 builder.Services.AddScoped<RemoveBannerCommandHandler>();
 
+builder.Services.AddScoped<GetBrandQueryHandler>();
+builder.Services.AddScoped<GetBrandByIdQueryHandler>();
+builder.Services.AddScoped<CreateBrandCommandHandler>();
+builder.Services.AddScoped<UpdateBrandCommandHandler>();
+builder.Services.AddScoped<RemoveBrandCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
